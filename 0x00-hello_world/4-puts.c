391e2da50883036b858e5a04b791749b13632169
Samiratu <samiratuntohsi@gmail.com>
2023-04-20 09:00:18 -0700

    added c program to print statement

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
+/* simple prit program */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
 }
